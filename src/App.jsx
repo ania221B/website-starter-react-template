369d623cad493f904a41e0e9e6bc8a89d89a6594
ania221B <100519699+ania221B@users.noreply.github.com>
2023-09-09 20:38:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App () {
+  return (
+    <main>
+      <section className='section'>
+        <div className='container'>
+          <h1>Vite + React + Sass</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import htmlPurge from 'vite-plugin-purgecss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), htmlPurge()]
+})
